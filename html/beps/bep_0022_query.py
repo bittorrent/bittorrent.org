43a12eea395ef2362f2c3dbd9766c30eff9b98a2
dave <dave@a0e1bbc1-f61f-0410-9ea2-93f5b4d9cef3>
2008-05-19 23:16:31 +0000

    bep 0022 cache discovery

---
+++ html/beps/bep_0022_query.py
@@ -0,0 +1,24 @@
+# Uses PyDNS.
+
+import DNS
+
+#req = DNS.Request( name="www.google.com", qtype="A", protocol= "udp", server="10.211.55.3" )
+#req = DNS.Request( name="www.google.com", qtype="A", protocol= "udp", server="127.0.0.1" )
+#req = DNS.Request( name="www.google.com", qtype="A", protocol= "udp", server="localhost" )
+#print "www.google.com: ", req.req().show()
+
+tlds = ["com", "net", "org"]  # add more here.
+
+name = DNS.revlookup( "69.107.0.14" )
+names = name.split('.')
+while names and names[0] not in tlds:
+   name = "_bittorrent._tcp." + ".".join(names)
+   req = DNS.Request( name=name, qtype="SRV", protocol="udp", server="localhost") 
+   response = req.req()
+   if response.answers:
+      break
+   del names[0]
+
+print "response=", response.show()   
+
+
